29ebae0a0722de23d1ee1cbd02b663687ce97913
Sajin Rana <132703827+sajin-rana@users.noreply.github.com>
2024-09-14 15:43:45 +0800

    fixing eslint error

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -3,14 +3,12 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { LuAlignLeft } from "react-icons/lu";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import UserIcon from "./UserIcon";
 import { links } from "@/utils/links";
-import SignOutLink from "./SignOutLink";
 
 function LinksDropdown() {
   return (
